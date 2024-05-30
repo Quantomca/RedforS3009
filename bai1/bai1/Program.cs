c2c2857c170b75f6c3f23466caadd2e7e4df49bf
Quantomca <146830328+Quantomca@users.noreply.github.com>
2024-05-30 19:47:18 +0700

    Add files via upload

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap vao mot so nguyen: ");
+        int number = int.Parse(Console.ReadLine());
+        double doubleNumber = (double)number;
+        Console.WriteLine($"So ban da nhap la {number} va duoi dang so thuc la {doubleNumber}");
+    }
+}
