e59a0c5c14d33bca8efbee3021b8a71b0e7f4bc9
Quantomca <146830328+Quantomca@users.noreply.github.com>
2024-05-30 19:48:12 +0700

    Add files via upload

---
+++ bai2/bai2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap vao mot so thuc: ");
+        double number = double.Parse(Console.ReadLine());
+        int intNumber = (int)number;
+        long longNumber = (long)number;
+        Console.WriteLine($"So ban da nhap la {number}, duoi dang so nguyen int la {intNumber} va duoi dang so nguyen long la {longNumber}");
+    }
+}
---
+++ bai3/bai3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap vao mot ki tu: ");
+        char character = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        int asciiCode = (int)character;
+        Console.WriteLine($"Ky tu ban da nhap la '{character}', ma ASCII la {asciiCode}");
+    }
+}
